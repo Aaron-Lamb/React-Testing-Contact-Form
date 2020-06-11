6d6bf1ad438388db6f54c7d2e7c57e3024666134
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-06-10 18:57:05 -0700

    Got one basic form running.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("finds input label", () => {
+  const { getByText } = render(<ContactForm />)
+  const form = getByText(/message/i)
+  expect(form).toBeInTheDocument();
+})
